19325644c289de8ddd92bbf9db792d2f48ccd92b
Mawa Ali <mawacoding1@gmail.com>
2024-06-13 08:37:17 -0400

    inital commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName;
+
+            dogName = "Piglet";
+
+            int dogAge = 5;
+
+            char firstInitial = 'P';
+
+            bool isHappy = true;
+
+            double dogWeight = 38.5;
+
+            decimal dogHeight = 12.5m;
+
+
+            Console.WriteLine($"Hello. My dog's name is {dogName}, his inital is {firstInitial} and he is {dogAge} years old.");
+            Console.WriteLine($"He weighs {dogWeight} pounds and he is {dogHeight} inches tall);
+            Console.WriteLine($"It is {isHappy} he is always happy!");
         }
     }
 }
